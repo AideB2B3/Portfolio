51ffe5d86fe1bedc8a84dfd92a7b9a6a11c48621
Davide Bellobuono <davide23bellobuono@gmail.com>
2026-01-21 13:13:53 +0100

    modifica sfondo

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -23,7 +23,7 @@ export const Hero = () => {
         <section className="relative min-h-screen flex items-center overflow-hidden">
             {/* background */}
             <div className="absolute inset-0"> 
-                <img src="/hero-bg.jpg" alt="Hero image" className="w-full h-full object-cover opacity-40" />
+                <img src="/hero-bg.png" alt="Hero image" className="w-full h-full object-cover opacity-40" />
                 <div className="absolute inset-0 bg-gradient-to-b from-background/20 via-background/80 to-background" />
             </div>
 
