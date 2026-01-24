4bd4cca0bf790888f0b1696eb227181c054f58f3
Davide Bellobuono <davide23bellobuono@gmail.com>
2026-01-24 12:51:01 +0100

    indirizzi

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -9,6 +9,7 @@ const footLinks = [
     { href: "#about", label: "About" },
     { href: "#projects", label: "Projects" },
     { href: "#experience", label: "Experience" },
+    {href: "#education", label: "Education" },
     { href: "#contact", label: "Contact" },
 ];
 
